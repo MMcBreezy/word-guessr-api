fa865d619d5237b641891f73c80a713e2e03ba3c
Robert Garmhausen <81274344+robertnopickle@users.noreply.github.com>
2023-01-13 19:33:59 -0500

    install jest and body-parser. we'll need those.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json());
 
 app.get('/heartbeat', (req, res) => {
   res.send('OK');

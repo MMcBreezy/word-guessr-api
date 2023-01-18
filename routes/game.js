a3c8fcd98cfc0e550ee24efcb691f6c42bb4380c
Robert Garmhausen <81274344+robertnopickle@users.noreply.github.com>
2023-01-17 22:24:05 -0500

    add POST and GET routes for /game

--- routes/game.js
+++ routes/game.js
@@ -1,16 +1,32 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const { Game } = require('../objects');
+const { Game } = require('../objects')
 
 // all routes in this file begin with /game
 
 // POST /game
 router.post('/', (req, res) => {
-  console.log('POST /game');
+  console.log('POST /game')
 
-  const game = new Game();
-  res.status(201).json({data: { game }});
+  // create and save a new game
+  const game = new Game()
+  req.games.set(game.id, game)
+
+  res.status(201).json({data: game.slim()})
 })
 
-module.exports = router;
+// GET /game/:game_id
+router.get('/:game_id', (req, res) => {
+  console.log('GET /game/:game_id');
+
+  const game = req.games.get(req.params.game_id)
+  if (game) {
+    res.json({data: game.slim()})
+  } else {
+    res.status(404).send()
+  }
+})
+
+
+module.exports = router

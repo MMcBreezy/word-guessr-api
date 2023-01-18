1944922b16f6dd91a44ef617b240fb423d56ba96
Robert Garmhausen <81274344+robertnopickle@users.noreply.github.com>
2023-01-17 22:24:05 -0500

    add POST and GET routes for /game

--- objects/game.js
+++ objects/game.js
@@ -1,11 +1,29 @@
+const uuid = require('uuid')
+
 const { wordHelper } = require('../helpers')
 
 class Game {
   constructor() {
-    this.word = wordHelper.getRandomWord()
+    const newWord = wordHelper.getRandomWord()
+    const letters = new Array(newWord.length).fill(null)
+
+    this.id = uuid.v4()
+    this.word = newWord
     this.guesses = []
+    this.letters = letters
     this.guessesRemaining = 6
   }
+
+  slim() {
+    return {
+      game_id: this.id,
+      letters: this.letters,
+      guesses: this.guesses,
+      guessesRemaining: this.guessesRemaining,
+    }
+  }
+
+
 }
 
 module.exports = Game

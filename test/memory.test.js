c99702b43f2fe764ab433e0e454ee788607db5d8
Robert Garmhausen <81274344+robertnopickle@users.noreply.github.com>
2023-01-21 20:42:27 -0500

    WIP, add an expiration to memory

--- test/memory.test.js
+++ test/memory.test.js
@@ -1,6 +1,14 @@
 const Memory = require('../memory');
 
+jest.useFakeTimers();
+let mem
+
 describe('Memory', () => {
+  beforeEach(() => {
+    mem = new Memory()
+    jest.clearAllTimers()
+  })
+
   it('should set and get a value', () => {
     const mem = new Memory();
     mem.set('foo', 'bar');
@@ -11,6 +19,23 @@ describe('Memory', () => {
     const mem = new Memory();
     mem.set('foo', 'bar');
     mem.delete('foo');
-    expect(mem.get('foo')).toEqual(undefined);
+    expect(mem.get('foo')).toBeNull();
   });
+
+  describe('expiration', () => {
+    const defaultExpiration = 259200000 // 3 days
+
+    it('should set an expiration time of 3 days', () => {
+      mem.set('foo', 'bar')
+      expect(mem.expiration('foo')).toEqual(Date.now() + defaultExpiration)
+    })
+
+    it('should renew the expiration time by three days if get is called', () => {
+      mem.set('foo', 'bar')
+      const advanceTimeBy = 10000
+      jest.advanceTimersByTime(advanceTimeBy)
+      mem.get('foo') 
+      expect(mem.expiration('foo')).toEqual(Date.now() + defaultExpiration)
+    })
+  })
 });

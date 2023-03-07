d26ed8b248c29a8c1bc0a98485fa135a4b0a3ac7
Robert Garmhausen <robertnopickle@github.com>
2023-03-06 20:55:59 -0500

    i hate these tests

--- test/helpers/wordHelper.test.js
+++ test/helpers/wordHelper.test.js
@@ -1,27 +1,25 @@
 const { wordHelper } = require('../../helpers')
 const fs = require('fs')
 
+let readFileSyncSpy
+let mathSpy
+
 beforeEach(() => {
-  jest.clearAllMocks()
+  readFileSyncSpy = jest.spyOn(fs, 'readFileSync').mockImplementation(() => 'foo\nbar\nbaz')
+  mathSpy = jest.spyOn(Math, 'random').mockImplementation(() => 0)
 })
+
+afterEach(() => jest.restoreAllMocks())
 
 describe('wordHelper', () => {
   describe('getRandomWord', () => {
-    it('should have a test that passes in this file', () => {
-      expect(true).toBe(true)
-    })
-    
     // TODO: fix this test
-    // it('should return a random word', () => {
-    //   const readFileSyncSpy = jest.spyOn(fs, 'readFileSync').mockReturnValue('foo\nbar\nbaz')
-    //   const mathSpy = jest.spyOn(Math, 'random').mockReturnValue(0)
+    it('should return a random word', () => {
+      const word = wordHelper.getRandomWord()
 
-    //   const word = wordHelper.getRandomWord()
-    //   console.log({ word })
-
-    //   expect(readFileSyncSpy).toHaveBeenCalledTimes(1)
-    //   expect(mathSpy).toHaveBeenCalledTimes(1)
-    //   expect(word).toEqual('foo')
-    // })
+      expect(readFileSyncSpy).toHaveBeenCalledTimes(1)
+      expect(mathSpy).toHaveBeenCalledTimes(1)
+      expect(word).toEqual('foo')
+    })
   })
 })

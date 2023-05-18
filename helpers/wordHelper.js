13a7ed65ad329cf590f90b01ad73ea8854a031bb
mamacbride <michaelmcbride85@gmail.com>
2023-05-17 23:04:07 -0400

    replaced word generation with openai API prompting

--- helpers/wordHelper.js
+++ helpers/wordHelper.js
@@ -1,18 +1,24 @@
-const fs = require('fs')
+require('dotenv').config();
+const { Configuration, OpenAIApi } = require('openai');
 
-const wordListPath = 'assets/wordList.txt'
-let words = null
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+});
+const openai = new OpenAIApi(configuration);
 
 class WordHelper {
-  static getRandomWord = () => {
-    if (!words) {
-      words = fs.readFileSync(wordListPath, 'utf-8').split("\n")
-    }
-    const randomWordIndex = Math.floor(Math.random() * words.length)
-
-    return words[randomWordIndex]
+  static getRandomWord = async () => {
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: 'Generate a random word suitable for a hangman game. The word should have a minimum of 5 letters and a maximum of 8 letters. Please provide a word that meets the criteria:',
+      max_tokens: 10,
+    });
+    
+    const word = response.data.choices[0].text.trim();
+    return word;
   }
 }
 
 
-module.exports = WordHelper
+
+module.exports = WordHelper;
